4f3196b31b4b043a787838f638b7174b31d1ace7
joaovitor101 <joao.kusaka27@gmail.com>
2025-03-29 19:32:40 -0300

    desenvolvimento parte header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css';    // Mantenha esta importação (estilos globais)
+import Header from './header';
+import './Header.module.css'; // Adicione esta linha (estilos do Header)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
